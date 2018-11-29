83cded5af73119e00c4e6500a0d4f969b158ea9a
Vimikp <vimikp@gmail.com>
2018-11-30 10:05:18 +1300

    Add project files.

---
+++ Loan WebServices/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Loan_WebServices
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
